9d9e1617be79ad25e9b5f8f7207ab3d246c9cbad
DanN918 <51687144+DanN918@users.noreply.github.com>
2020-06-01 12:55:10 -0400

    Updates

---
+++ precache-manifest.3b56f327425b7e3fee66cf5fb639f4ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ef4a806af085e14a596059b5793406",
+    "url": "/%7Bpro-finish-nails%7D/index.html"
+  },
+  {
+    "revision": "bce2f52d7cacb17430b6",
+    "url": "/%7Bpro-finish-nails%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "702a8c6dc921483e5d48",
+    "url": "/%7Bpro-finish-nails%7D/static/js/2.313072b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Bpro-finish-nails%7D/static/js/2.313072b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce2f52d7cacb17430b6",
+    "url": "/%7Bpro-finish-nails%7D/static/js/main.2b522388.chunk.js"
+  },
+  {
+    "revision": "f12b2ddf72e913e713ed",
+    "url": "/%7Bpro-finish-nails%7D/static/js/runtime-main.c15c1504.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7Bpro-finish-nails%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
